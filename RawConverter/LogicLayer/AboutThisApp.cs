10ba3fe0d89af1c63b19c91c2e52c4bfab612bdd
Ludwig Sembach <ludwig.sembach@XPS15>
2021-07-03 23:47:53 +0200

    NuGet updated, ready for release 1.0.1

--- RawConverter/LogicLayer/AboutThisApp.cs
+++ RawConverter/LogicLayer/AboutThisApp.cs
@@ -3,7 +3,7 @@
     static class AboutThisApp
     {
         public static readonly string name = "Raw Converter";
-        public static readonly string version = "1.0.0";
-        public static readonly string buildDate = "2021-06-25";
+        public static readonly string version = "1.0.1";
+        public static readonly string buildDate = "2021-07-03";
     }
 }
