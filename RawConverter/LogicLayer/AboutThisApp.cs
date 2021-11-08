5122d424d65060a1d37ba095b7b5c54d492c128a
Ludwig Sembach <ludwig.sembach@outlook.com>
2021-11-08 22:25:02 +0100

    ready for release 1.0.3

--- RawConverter/LogicLayer/AboutThisApp.cs
+++ RawConverter/LogicLayer/AboutThisApp.cs
@@ -3,7 +3,7 @@
     static class AboutThisApp
     {
         public static readonly string name = "Raw Converter";
-        public static readonly string version = "1.0.2";
-        public static readonly string buildDate = "2021-07-06";
+        public static readonly string version = "1.0.3";
+        public static readonly string buildDate = "2021-11-08";
     }
 }
