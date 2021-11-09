a60e84071c87f4769689ec63b133d882f27b51d9
Ludwig Sembach <ludwig.sembach@outlook.com>
2021-11-09 23:52:39 +0100

    about file updated with wrong extension in file name bug

--- RawConverter/LogicLayer/AboutThisApp.cs
+++ RawConverter/LogicLayer/AboutThisApp.cs
@@ -3,7 +3,7 @@
     static class AboutThisApp
     {
         public static readonly string name = "Raw Converter";
-        public static readonly string version = "1.0.3";
-        public static readonly string buildDate = "2021-11-08";
+        public static readonly string version = "1.0.4"; // not released yet!
+        public static readonly string buildDate = "2021-11-xx";
     }
 }
