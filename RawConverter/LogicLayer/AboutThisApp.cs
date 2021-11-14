d5a85b49319143552ee5d11b1f085cb711b5793d
Ludwig Sembach <ludwig.sembach@outlook.com>
2021-11-14 13:47:33 +0100

    bug fix: image in menu button no longer disappears while mouse is over button

--- RawConverter/LogicLayer/AboutThisApp.cs
+++ RawConverter/LogicLayer/AboutThisApp.cs
@@ -3,7 +3,7 @@
     static class AboutThisApp
     {
         public static readonly string name = "Raw Converter";
-        public static readonly string version = "1.0.4";
-        public static readonly string buildDate = "2021-11-12";
+        public static readonly string version = "1.x.y"; // current version is 1.0.4
+        public static readonly string buildDate = "202z-yy-xx";
     }
 }
